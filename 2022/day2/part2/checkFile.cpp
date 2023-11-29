9e0663cd9311444e0161d0c8f2541f357f276589
Alexandre Voinier <alexandrevoinier34@gmail.com>
2023-11-29 11:27:29 +0100

    [2022] - Day 2 part 2

---
+++ 2022/day2/part2/checkFile.cpp
@@ -0,0 +1,16 @@
+#include <filesystem>
+
+#include "checkFile.h" 
+
+bool checkFile(std::string path)
+{
+  std::filesystem::path filepath {path};
+  if (std::filesystem::exists(filepath))
+  {
+    if (std::filesystem::is_regular_file(filepath))
+    {
+      return true;
+    }
+  }
+  return false;
+}
