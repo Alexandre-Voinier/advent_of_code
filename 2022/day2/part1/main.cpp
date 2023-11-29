2cee6ebe1529885fd5c30dce2e6c80edc3f8f4e9
Alexandre Voinier <alexandrevoinier34@gmail.com>
2023-11-29 11:16:27 +0100

    [2022] - Day 2 part 1

---
+++ 2022/day2/part1/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "checkFile.h"
+
+int roundScore(char elf, char player)
+{
+  if (elf == 'A')
+  {
+    if (player == 'X')
+      return 4;
+    else if (player == 'Y')
+      return 8;
+    else
+      return 3;
+  }
+  else if (elf == 'B')
+  {
+    if (player == 'X')
+      return 1;
+    else if (player == 'Y')
+      return 5;
+    else
+      return 9;
+  } 
+  else
+{
+    if (player == 'X')
+      return 7;
+    else if (player == 'Y')
+      return 2;
+    else
+      return 6;
+  }
+}
+
+int main (int argc, char *argv[])
+{  
+  if (argc != 2)
+  {
+    std::cout << "Usage: day1 <inputFile>"  << std::endl;
+    return 1;
+  }
+
+  if (checkFile(argv[1]))
+  {
+
+    std::ifstream file {argv[1]};
+    if (!file) {
+      std::cerr << "Error opening the file: " 
+        << argv[1]
+        << std::endl;
+    }
+    std::string s;
+    int score {};
+    while (getline(file, s))
+    {
+      if (s.size() > 2)
+      {
+        char elf {s[0]};
+        char player {s[2]};
+        score += roundScore(elf, player);
+      }
+    }
+    std::cout << "Score: " << score << std::endl;
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE; 
+}
